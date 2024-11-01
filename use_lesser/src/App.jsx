53b8c180de5d9b0db9a76bc17dfd5e0393fcfce9
Voidknight-2004 <rohanajacobgdsc@gmail.com>
2024-11-01 18:25:22 +0530

    tailwind fix

--- use_lesser/src/App.jsx
+++ use_lesser/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +5,7 @@ function App() {
   return (
    
     <div>
-      hello
+      <h1 className='text-red-500'>hello</h1>
     </div>
   )
 }
